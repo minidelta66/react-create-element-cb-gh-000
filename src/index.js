2d57f676a75ea2c8a052e626569b89d7e1b159cf
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-04-08 19:25:13 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 // Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = React.createElement('h1', {}, 'My First React Code');
+
+ReactDOM.render(
+  title,
+  document.getElementById('global')
+);
